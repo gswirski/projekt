b3d7d85a773ba2d542e09fe9e3fae55e22aee03e
Grzegorz Świrski <grzegorz@swirski.name>
2015-01-23 21:56:12 +0100

    dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,32 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Recent Meals" do
+          ul do
+            Meal.where(user_id: current_user.id).order("id desc").limit(5).map do |meal|
+              li link_to(I18n.l(meal.date, format: :short), meal_path(meal))
+            end
+          end
+          para do
+            link_to "Add Meal", new_meal_path, class: "button"
+          end
+        end
+      end
+
+      column do
+        panel "Recent Recipes" do
+          ul do
+            Recipe.where(user_id: current_user.id).order("id desc").limit(5).map do |recipe|
+              li link_to(recipe.name, recipe_path(recipe))
+            end
+          end
+          para do
+            link_to "Add Recipe", new_recipe_path, class: "button"
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
